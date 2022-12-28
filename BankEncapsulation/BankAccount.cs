d8920b07cbdb18560b08bacbb670448299c37bfd
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2022-12-28 00:12:46 -0700

    Completed Exercise 1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+
+        //Create a private field of type double named balance with a value of 0
+        private double balance = 0;
+
+
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        public void Deposit(double amount)
+		{
+			balance = amount;
+		}
+
+        //Define a method named GetBalance that will return the amount stored in the balance field
+        public string GetBalance()
+		{
+			return $"${balance}";
+		}
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var brandonsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            brandonsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank You! Your balance is now {brandonsAccount.GetBalance()}");
+
         }
     }
 }
